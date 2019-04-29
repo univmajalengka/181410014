a56f23c9187731c8f4f5203ec65c6556719f14a3
irnasrinulingga <50064742+irnasrinulingga@users.noreply.github.com>
2019-04-29 10:39:33 +0700

    Add files via upload

---
+++ STD1/irna-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+char provinsi[3][12]={"jawa barat", "jawa timur", "jawa tengah"};
+int n;
+int main() 
+{
+
+	cout<<"Masukkan data array : ";
+	cin>>n;
+	cout<<"Data yang dimasukkan : ";
+		cout<<" "<<provinsi[n];
+	return 0;
+}
