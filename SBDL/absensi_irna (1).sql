c578af3197f0871f64571cf47f39d5ca75cb8f39
irnasrinulingga <50064742+irnasrinulingga@users.noreply.github.com>
2020-04-03 13:36:11 +0700

    Update Database Absensi

--- SBDL/absensi_irna (1).sql
+++ SBDL/absensi_irna (1).sql
@@ -1,162 +1,348 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20 Mar 2020 pada 10.32
--- Versi Server: 5.6.21
--- PHP Version: 5.6.3
+-- Waktu pembuatan: 03 Apr 2020 pada 08.35
+-- Versi server: 10.4.10-MariaDB
+-- Versi PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `absensi_irna`
+-- Database: `absensi_siswa`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `master_guru`
+-- Struktur dari tabel `bulan`
 --
 
-CREATE TABLE IF NOT EXISTS `master_guru` (
-  `kode_guru` varchar(10) NOT NULL,
-  `nip` varchar(10) NOT NULL,
-  `nama_guru` text NOT NULL,
-  `jk` text NOT NULL,
-  `alamat` text NOT NULL,
-  `password` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `bulan` (
+  `id_bln` int(10) NOT NULL,
+  `nama_bln` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `master_guru`
+-- Dumping data untuk tabel `bulan`
 --
 
-INSERT INTO `master_guru` (`kode_guru`, `nip`, `nama_guru`, `jk`, `alamat`, `password`) VALUES
-('11', '11111111', 'Muhamad Syarif', 'Laki - laki', 'Palasah', '45475'),
-('22', '22222222', 'Apriyani', 'Perempuan', 'Weragati', '45476'),
-('33', '33333333', 'Reza Dwi', 'Perempuan', 'Trajaya', '45477');
+INSERT INTO `bulan` (`id_bln`, `nama_bln`) VALUES
+(1, 'Januari'),
+(2, 'Februari'),
+(3, 'Maret'),
+(4, 'April'),
+(5, 'Mei'),
+(6, 'Juni'),
+(7, 'Juli'),
+(8, 'Agustus'),
+(9, 'September'),
+(10, 'Oktober'),
+(11, 'November'),
+(12, 'Desember');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `master_jurusan`
+-- Struktur dari tabel `catatan`
 --
 
-CREATE TABLE IF NOT EXISTS `master_jurusan` (
-  `kode_jurusan` varchar(10) NOT NULL,
-  `jurusan` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `catatan` (
+  `id_cat` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `id_bln` int(10) NOT NULL,
+  `id_hri` int(10) NOT NULL,
+  `id_tgl` int(10) NOT NULL,
+  `isi_cat` longtext NOT NULL,
+  `status_cat` enum('Menunggu','Dikonfirmasi','Ditolak') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `master_jurusan`
+-- Dumping data untuk tabel `catatan`
 --
 
-INSERT INTO `master_jurusan` (`kode_jurusan`, `jurusan`) VALUES
-('20', 'Farmasi'),
-('21', 'Perawat'),
-('22', 'Akuntansi');
+INSERT INTO `catatan` (`id_cat`, `id_user`, `id_bln`, `id_hri`, `id_tgl`, `isi_cat`, `status_cat`) VALUES
+(60, 12, 4, 5, 3, 'Materi pertemuan 2', 'Dikonfirmasi'),
+(59, 9, 4, 5, 3, 'Materi SBDL ke 2', 'Dikonfirmasi'),
+(58, 11, 4, 5, 3, 'Materi pertemuan ke 2', 'Dikonfirmasi'),
+(55, 11, 4, 4, 2, 'augfiawgfawfqw', 'Ditolak'),
+(56, 10, 4, 5, 3, 'Materi Sistem Basis Data Lanjut', 'Dikonfirmasi'),
+(57, 8, 4, 5, 3, 'Materi SBDL', 'Dikonfirmasi'),
+(52, 9, 4, 4, 2, 'tidak ada materi', 'Dikonfirmasi'),
+(53, 11, 4, 4, 2, 'absensi saja', 'Dikonfirmasi'),
+(51, 10, 4, 4, 2, 'Materi pertemuan pertama', 'Dikonfirmasi'),
+(50, 8, 4, 4, 2, 'Percobaan absensi siswa', 'Dikonfirmasi');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `master_kelas`
+-- Struktur dari tabel `data_absen`
 --
 
-CREATE TABLE IF NOT EXISTS `master_kelas` (
-  `no` varchar(10) NOT NULL,
-  `nama_kelas` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `data_absen` (
+  `id_absen` int(11) NOT NULL,
+  `id_user` varchar(100) NOT NULL,
+  `id_bln` int(10) NOT NULL,
+  `id_hri` int(10) NOT NULL,
+  `id_tgl` int(10) NOT NULL,
+  `jam_msk` varchar(50) NOT NULL,
+  `st_jam_msk` enum('Menunggu','Dikonfirmasi','Ditolak') NOT NULL,
+  `jam_klr` varchar(50) NOT NULL,
+  `st_jam_klr` enum('Belum Absen','Menunggu','Dikonfirmasi','Ditolak') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `master_kelas`
+-- Dumping data untuk tabel `data_absen`
 --
 
-INSERT INTO `master_kelas` (`no`, `nama_kelas`) VALUES
-('01', ' Farm'),
-('02', 'Kep'),
-('03', 'AK');
+INSERT INTO `data_absen` (`id_absen`, `id_user`, `id_bln`, `id_hri`, `id_tgl`, `jam_msk`, `st_jam_msk`, `jam_klr`, `st_jam_klr`) VALUES
+(73, '8', 4, 5, 3, '12.51 WIB', 'Dikonfirmasi', '12.52 WIB', 'Dikonfirmasi'),
+(72, '10', 4, 5, 3, '12.50 WIB', 'Dikonfirmasi', '12.51 WIB', 'Dikonfirmasi'),
+(71, '12', 4, 4, 2, '13.04 WIB', 'Dikonfirmasi', '13.04 WIB', 'Dikonfirmasi'),
+(70, '11', 4, 4, 2, '13.03 WIB', 'Ditolak', '13.03 WIB', 'Ditolak'),
+(69, '9', 4, 4, 2, '13.02 WIB', 'Dikonfirmasi', '13.03 WIB', 'Ditolak'),
+(68, '10', 4, 4, 2, '12.57 WIB', 'Dikonfirmasi', '12.57 WIB', 'Dikonfirmasi'),
+(67, '8', 4, 4, 2, '12.50 WIB', 'Dikonfirmasi', '12.51 WIB', 'Ditolak'),
+(74, '11', 4, 5, 3, '12.52 WIB', 'Dikonfirmasi', '12.53 WIB', 'Dikonfirmasi'),
+(75, '9', 4, 5, 3, '12.53 WIB', 'Dikonfirmasi', '12.54 WIB', 'Dikonfirmasi'),
+(76, '12', 4, 5, 3, '12.55 WIB', 'Dikonfirmasi', '12.55 WIB', 'Dikonfirmasi');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `master_mata_pelajaran`
+-- Struktur dari tabel `detail_pb`
 --
 
-CREATE TABLE IF NOT EXISTS `master_mata_pelajaran` (
-  `kode_mp` varchar(10) NOT NULL,
-  `nama_mp` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `detail_pb` (
+  `id_user` int(10) NOT NULL,
+  `name_user` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `master_mata_pelajaran`
+-- Dumping data untuk tabel `detail_pb`
 --
 
-INSERT INTO `master_mata_pelajaran` (`kode_mp`, `nama_mp`) VALUES
-('31', 'Indonesia'),
-('32', 'Inggris'),
-('33', 'KWU');
+INSERT INTO `detail_pb` (`id_user`, `name_user`) VALUES
+(7, 'Administrator');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `master_siswa`
+-- Struktur dari tabel `detail_user`
 --
 
-CREATE TABLE IF NOT EXISTS `master_siswa` (
-  `kode_siswa` varchar(10) NOT NULL,
-  `nis_siswa` varchar(10) NOT NULL,
-  `nama_siswa` varchar(20) NOT NULL,
-  `jk` varchar(10) NOT NULL,
-  `kelas` varchar(10) NOT NULL,
-  `jurusan` varchar(20) NOT NULL,
-  `nama_orangtua` varchar(10) NOT NULL,
-  `nomor_hp` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `detail_user` (
+  `id_user` int(10) NOT NULL,
+  `nis_user` int(25) NOT NULL,
+  `name_user` varchar(255) NOT NULL,
+  `sklh_user` varchar(255) NOT NULL,
+  `jk_user` varchar(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `master_siswa`
+-- Dumping data untuk tabel `detail_user`
 --
 
-INSERT INTO `master_siswa` (`kode_siswa`, `nis_siswa`, `nama_siswa`, `jk`, `kelas`, `jurusan`, `nama_orangtua`, `nomor_hp`) VALUES
-('55', '435672', 'Nisa Rahmadini', 'Perempuan', 'Farm', 'Farmasi', 'Yahya', '0897674381'),
-('56', '984271', 'Muhamad Alif', 'Laki Laki', 'AK', 'Akuntansi', 'Dirman', '0821763876'),
-('57', '984274', 'Rina Febriani', 'Perempuan', 'Kep', 'Perawat', 'Ujang', '0812764934');
+INSERT INTO `detail_user` (`id_user`, `nis_user`, `name_user`, `sklh_user`, `jk_user`) VALUES
+(8, 181410014, 'Irna Sri Nulingga', 'Informatika', 'P'),
+(9, 87564, 'Muhammad Hikayat', 'Informatika', 'L'),
+(10, 902734, 'Sandra Marlianti M', 'Informatika', 'P'),
+(11, 8236782, 'Afif Surya Ramadhan', 'Informatika', 'L'),
+(12, 86764534, 'Rizki Alam Ramdhani', 'Informatika', 'L');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hari`
+--
+
+CREATE TABLE `hari` (
+  `id_hri` int(10) NOT NULL,
+  `nama_hri` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `hari`
+--
+
+INSERT INTO `hari` (`id_hri`, `nama_hri`) VALUES
+(1, 'Senin'),
+(2, 'Selasa'),
+(3, 'Rabu'),
+(4, 'Kamis'),
+(5, 'Jum\'at'),
+(6, 'Sabtu'),
+(7, 'Minggu');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tanggal`
+--
+
+CREATE TABLE `tanggal` (
+  `id_tgl` int(10) NOT NULL,
+  `nama_tgl` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tanggal`
+--
+
+INSERT INTO `tanggal` (`id_tgl`, `nama_tgl`) VALUES
+(1, '01'),
+(2, '02'),
+(3, '03'),
+(4, '04'),
+(5, '05'),
+(6, '06'),
+(7, '07'),
+(8, '08'),
+(9, '09'),
+(10, '10'),
+(11, '11'),
+(12, '12'),
+(13, '13'),
+(14, '14'),
+(15, '15'),
+(16, '16'),
+(17, '17'),
+(18, '18'),
+(19, '19'),
+(20, '20'),
+(21, '21'),
+(22, '22'),
+(23, '23'),
+(24, '24'),
+(25, '25'),
+(26, '26'),
+(27, '27'),
+(28, '28'),
+(29, '29'),
+(30, '30'),
+(31, '31');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `email_user` varchar(255) NOT NULL,
+  `pwd_user` varchar(255) NOT NULL,
+  `level_user` enum('sw','pb') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `email_user`, `pwd_user`, `level_user`) VALUES
+(11, 'afifsurya@gmail.com', 'b56776aa98086825550ff0c3fe260907', 'sw'),
+(8, 'irnasri838@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'sw'),
+(7, 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'pb'),
+(9, 'hikayat42@gmail.com', '894cacb65ca6e69e2996f2151cfeeea5', 'sw'),
+(10, 'sandramarlianti@gmail.com', 'f40a37048732da05928c3d374549c832', 'sw'),
+(12, 'rizkialam@gmail.com', '3e089c076bf1ec3a8332280ee35c28d4', 'sw');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `master_guru`
+-- Indeks untuk tabel `bulan`
 --
-ALTER TABLE `master_guru`
- ADD PRIMARY KEY (`kode_guru`);
+ALTER TABLE `bulan`
+  ADD PRIMARY KEY (`id_bln`);
 
 --
--- Indexes for table `master_jurusan`
+-- Indeks untuk tabel `catatan`
 --
-ALTER TABLE `master_jurusan`
- ADD PRIMARY KEY (`kode_jurusan`);
+ALTER TABLE `catatan`
+  ADD PRIMARY KEY (`id_cat`);
 
 --
--- Indexes for table `master_mata_pelajaran`
+-- Indeks untuk tabel `data_absen`
 --
-ALTER TABLE `master_mata_pelajaran`
- ADD PRIMARY KEY (`kode_mp`);
+ALTER TABLE `data_absen`
+  ADD PRIMARY KEY (`id_absen`);
 
 --
--- Indexes for table `master_siswa`
+-- Indeks untuk tabel `detail_pb`
 --
-ALTER TABLE `master_siswa`
- ADD PRIMARY KEY (`kode_siswa`);
+ALTER TABLE `detail_pb`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `detail_user`
+--
+ALTER TABLE `detail_user`
+  ADD PRIMARY KEY (`id_user`,`nis_user`);
+
+--
+-- Indeks untuk tabel `hari`
+--
+ALTER TABLE `hari`
+  ADD PRIMARY KEY (`id_hri`);
+
+--
+-- Indeks untuk tabel `tanggal`
+--
+ALTER TABLE `tanggal`
+  ADD PRIMARY KEY (`id_tgl`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`,`email_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `bulan`
+--
+ALTER TABLE `bulan`
+  MODIFY `id_bln` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `catatan`
+--
+ALTER TABLE `catatan`
+  MODIFY `id_cat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT untuk tabel `data_absen`
+--
+ALTER TABLE `data_absen`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT untuk tabel `hari`
+--
+ALTER TABLE `hari`
+  MODIFY `id_hri` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tanggal`
+--
+ALTER TABLE `tanggal`
+  MODIFY `id_tgl` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
